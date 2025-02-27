c769750eb6bf897bb645501954267ec0c40f1c1c
JeongSangMyeong <ajflsp@naver.com>
2025-02-27 14:21:23 +0900

    상품 등록 처리 - @ModelAttribute

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
@@ -43,10 +45,44 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
-    @PostMapping("/add")
-    public String save() {
+    // @PostMapping("/add")
+    public String addItemV1(
+            @RequestParam("itemName") String itemName,
+            @RequestParam("price") int price,
+            @RequestParam("quantity") Integer quantity,
+            Model model) {
 
-        return "XXXX";
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+        model.addAttribute("item", item);
+
+        return "/basic/item";
+    }
+
+    // @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+
+        itemRepository.save(item);
+        // model.addAttribute("item", item); // 자동 추가, 생략 가능
+
+        return "/basic/item";
+    }
+
+    // @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        return "/basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        // Item -> item
+        itemRepository.save(item);
+        return "/basic/item";
     }
 
     /*
